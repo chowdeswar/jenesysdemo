1e90585686d0627b65ea2db693bc6227c92be845
Chowdeswara Rao <chowdeswara.rao@jenesystech.com>
2022-04-13 16:36:55 +0530

    url changes

--- src/app/components/pages/partner/partner.component.ts
+++ src/app/components/pages/partner/partner.component.ts
@@ -1,18 +1,24 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-partner',
-  templateUrl: './partner.component.html',
-  styleUrls: ['./partner.component.scss']
+    selector: 'app-partner',
+    templateUrl: './partner.component.html',
+    styleUrls: ['./partner.component.scss']
 })
 export class PartnerComponent implements OnInit {
+    locationPath: string;
 
-  constructor(private titleCap: Title) { }
+    constructor(private titleCap: Title, private location: Location) { }
 
-  ngOnInit(): void {
-    this.titleCap.setTitle('Partner Solutions - Jenesys Technologies');
+    ngOnInit(): void {
+        this.titleCap.setTitle('Partner Solutions - Jenesys Technologies');
+        // this.locationPath = window.location.protocol + '' + window.location.host;
+        // this.location.subscribe(res => {
+        // console.log(this.locationPath);
+        // });
 
-  }
+    }
 
 }
